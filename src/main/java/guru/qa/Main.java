949fd776ebe94444b34dd566bc4199b133d06ce2
Eldar Murzakhanov <125517700+EldarMurzakhanov@users.noreply.github.com>
2023-02-17 13:36:31 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru and java stream 18!");
+        System.out.println("Hello qa.guru and java stream 18!!!!!");
+      
     }
 }
